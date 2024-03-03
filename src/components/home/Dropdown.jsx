23aaadb40bcf4183d63ab08654ffcb438f135c64
Kimberly Howard <kimberlyhoward529@gmail.com>
2024-03-03 11:29:32 -0500

    Tried to create an object to hold list and category data

--- src/components/home/Dropdown.jsx
+++ src/components/home/Dropdown.jsx
@@ -2,7 +2,7 @@ import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
-function Dropdown (props) {
+const Dropdown = (props) => {
     const [isActive, setIsActive] = useState(false);
     const [newCategory, setNewCategory] = useState("");
 
@@ -12,42 +12,50 @@ function Dropdown (props) {
 
     const items = ["Produce", "Dairy", "Canned", "Pasta", "Bread", "Meat", "Snacks", "Frozen"];
 
-    const handleDropdownItemClick = (e) => {
+    const handleChange = (e) => {
         setNewCategory(e.target.textContent);
     }
 
-    const handleClick = () => {
-        props.onClick(newCategory)
+    // console.log("CATEGORY: ", newCategory)
+
+    console.log("NEW CATEGORY ", newCategory)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("NEW CATEGORY: ", newCategory)
+        props.onSubmit(newCategory);
     }
 
     return (
-        <div className="relative mx-auto z-0" onClick={handleClick}>
-            <div 
-                className="mx-2 py-3.5 px-5 bg-gray-700 bg-opacity-40 justify-between text-white cursor-pointer flex items-center w-48 rounded-xl text-2xl border-2" 
-                onClick={toggleBtn}
-            >
-                <h1 className="font-bold">Category</h1>
-                {isActive ? (
-                    <FontAwesomeIcon icon={faAngleUp} />
-                ) : (
-                    <FontAwesomeIcon icon={faAngleDown} />
-                )}
-            </div>
-            {isActive && (
-                <div className="absolute top-[100%] p-2.5 bg-white text-gray-600 mx-2 mt-0 w-48 rounded-xl">
-                    {items.map((item, index) => {
-                        return (
-                            <div 
-                                key={index} 
-                                className="dropdown-item p-2.5 hover:bg-gray-700 hover:bg-opacity-40 hover:text-white cursor-pointer transition-all text-xl"
-                                onClick={handleDropdownItemClick}
-                            >
-                                {item}
-                            </div>
-                        )
-                    })}
+        <div>
+            <form className="relative mx-auto z-0" onSubmit={handleSubmit}>
+                <div 
+                    className="mx-2 py-3.5 px-5 bg-gray-700 bg-opacity-40 justify-between text-white cursor-pointer flex items-center w-48 rounded-xl text-2xl border-2" 
+                    onClick={toggleBtn}
+                >
+                    <h1 className="font-bold">Category</h1>
+                    {isActive ? (
+                        <FontAwesomeIcon icon={faAngleUp} />
+                    ) : (
+                        <FontAwesomeIcon icon={faAngleDown} />
+                    )}
                 </div>
-            )}
+                {isActive && (
+                    <div className="absolute top-[100%] p-2.5 bg-white text-gray-600 mx-2 mt-0 w-48 rounded-xl">
+                        {items.map((item, index) => {
+                            return (
+                                <div 
+                                    key={index} 
+                                    className="dropdown-item p-2.5 hover:bg-gray-700 hover:bg-opacity-40 hover:text-white cursor-pointer transition-all text-xl"
+                                    onClick={handleChange}
+                                >
+                                    {item}
+                                </div>
+                            )
+                        })}
+                    </div>
+                )}
+            </form>
         </div>
     )
 }
--- src/components/home/createList.jsx
+++ src/components/home/createList.jsx
@@ -1,38 +1,28 @@
 import { useState, useEffect } from "react";
 import { db } from "../../firebase/firebase";
 import { collection, getDocs, addDoc } from "firebase/firestore"
-import Dropdown from "./Dropdown"
-
-console.log("Get Category ")
 
 const CreateList = () => {
     const [listItems, setListItems] = useState([]);
     const [newItem, setNewItem] = useState("");
     const [newCategory, setNewCategory] = useState("");
-    const [isClicked, setIsClicked] = useState(false);
-    const [isClickedIndex, setIsClickedIndex] = useState(0);
-    const [isItemCrossed, setIsItemCrossed] = useState();
+    const [categories, setCategories] = useState([]);
     const listItemsCollectionRef = collection(db, "groceryList");
-
-    // console.log("LIST ITEMS: ", listItems);
+    const listData = {};
 
     const createItem = async () => {
-        await addDoc(listItemsCollectionRef, {item: newItem})
+        await addDoc(listItemsCollectionRef, {name: newItem, category: newCategory});
         location.reload();
     }
 
-    const getCategory = (category) => {
-        console.log("SELECTED CATEGORY: ", category);
-        setNewCategory(category);
+    const handleCategoryChange = (e) => {
+        setNewCategory(e.target.value);
     }
-
-    console.log("CURRENT CATEGORY: ", newCategory)
 
     useEffect(() => {
         const getListItems =  async () => {
             const data = await getDocs(listItemsCollectionRef);
             setListItems(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-            // console.log("LIST ITEMS: ", listItems)
         }
 
         getListItems();
@@ -48,6 +38,21 @@ const CreateList = () => {
        }
     }
 
+    listItems.map((item) => {
+        // if(!categories.includes(item.category.toUpperCase())) {
+        //     categories.push(item.category.toUpperCase());
+        // }
+        let label = item.category.toUpperCase();
+        console.log("LABEL: ", label)
+        if(label in listData) {
+            listData.label.push(item.name);
+        } else {
+            Object.assign(listData, {label: []});
+        }
+    })
+
+    console.log("LIST DATA: ", listData)
+
     return (
         <div className="flex flex-col">
             <h1 className="text-5xl text-white">Welcome! Add a grocery item</h1>
@@ -59,7 +64,11 @@ const CreateList = () => {
                         setNewItem(e.target.value)
                     }} 
                 />
-                <Dropdown onClick={getCategory}/>
+                {/* <Dropdown onSubmit={getCategory}/> */}
+                <input 
+                    className="py-3.5 px-5 rounded-xl mx-2 w-full text-2xl bg-gray-700 bg-opacity-40 text-white placeholder:text-white border-2" 
+                    placeholder="Category..."
+                    onChange={handleCategoryChange} />
                 <button 
                     className="border-2 bg-teal-500 opactiy-40 p-2 rounded-xl text-white hover:bg-teal-400 text-2xl font-bold mx-2"
                     onClick={createItem}
@@ -67,22 +76,37 @@ const CreateList = () => {
                     ADD
                 </button>
             </div>
-            <div className="self-center border-2 p-5 bg-gray-700 bg-opacity-40 rounded-3xl m-24">
-                {" "}
-                {listItems.map((listItem, index) => {
-                    return (
-                        <div key={listItem.id}>
-                            <div className="rounded-xl p-3 text-2xl w-96 text-white" onClick={handleClick}>
-                                <div className="flex">
-                                    <p> - </p> 
-                                    <p className="pl-2">{listItem.item}</p>
+            {listItems.length > 0 && (
+                <div className="self-center border-2 p-5 bg-gray-700 bg-opacity-40 rounded-3xl m-24">
+                    {" "}
+                    {listItems.map((listItem, index) => {
+                        return (
+                            <div key={listItem.id}>
+                                <div className="rounded-xl p-3 text-2xl w-96 text-white" onClick={handleClick}>
+                                    {categories.map((category) => {
+                                        return (
+                                            <div key={category}>
+                                                {listItem.category.toUpperCase() === category && (
+                                                    <div className="flex flex-col text-white">
+                                                        <div>
+                                                            <p className="font-bold">{category}</p>
+                                                        </div>
+                                                        <div className="flex">
+                                                            <p> - </p> 
+                                                            <p className="pl-2">{listItem.name}</p>
+                                                        </div>
+                                                    </div>
+                                                )}       
+                                            </div>
+                                        )
+                                    })}
                                 </div>
                             </div>
-                        </div>
-                    )
-                })} 
-                <button className="bg-teal-500 bg-opactiy-20 p-2 rounded-xl text-white hover:bg-teal-400 text-2xl font-bold my-5 mx-3 border-2" >SAVE</button>
-            </div>
+                        )
+                    })} 
+                    <button className="bg-teal-500 bg-opactiy-20 p-2 rounded-xl text-white hover:bg-teal-400 text-2xl font-bold my-5 mx-3 border-2" >SAVE</button>
+                </div>
+            )}
         </div>
     )
 }
